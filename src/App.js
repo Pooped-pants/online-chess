acd9288739c37be556e4fb80c13c754c74408ad1
KlassiC-Man <parth.11.gulati@gmail.com>
2021-07-19 10:49:34 +0530

    Login Page structure completed style remains

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen/SignUpScreen';
 
 function App() {
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route path='/'>
-                        <LoginScreen />
-                    </Route>
+                    <Route path='/' exact component={LoginScreen} />
+                    <Route path='/signup' component={SignUpScreen} />
                 </Switch>
             </div>
         </Router>
--- src/screens/LoginScreen/components/LoginBody.js
+++ src/screens/LoginScreen/components/LoginBody.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import './LoginBody.css';
 import {auth, db} from '../../../firebase';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 function LoginBody() {
     const [username, setUsername] = useState('');
@@ -11,17 +17,15 @@ function LoginBody() {
         auth.signInWithEmailAndPassword(email, password).catch(error => alert(error));
     };
 
-    function signUp() {
-        // Will navigate to the sign Up page tomm!!!
-    }
-
     return (
         <div className='loginbody'>
             <form className='loginbodyForm'>
                 <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder='Username' className='loginbodyFormInput' />
                 <input type='email' value={email} onChange={e => setEmail(e.target.value)} placeholder='Email' className='loginbodyFormInput' />
-                <input type='password' value={password} onChangeText={e => setPassword(e.target.value)} placeholder='Password' className='loginbodyFormInput' />
-                <input type='submit' value='Sign Up' className='signUp' />
+                <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Password' className='loginbodyFormInput' />
+                <Link to='/signup'>
+                    <input type='submit' value='Sign Up' className='signUp' />
+                </Link>
                 <input type='submit' value='Login' className='login' onClick={login} />
             </form>
         </div>
