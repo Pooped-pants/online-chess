6711be61024c85f0600f4dd15898282d6f1599c0
kaneki <jksheth46@gmail.com>
2021-07-19 20:17:29 +0530

    changed PlayScreen

---
+++ src/screens/PlayScreen/components/GameEnd.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PlayGame from './PlayGame';
+import "./GameEnd.css";
+
+function GameEnd() {
+    return (
+        <div>
+            <div className='Re-Match'>
+
+            </div>
+            <div className='Home-Screen'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default GameEnd
---
+++ src/screens/PlayScreen/components/PlayGame.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function PlayGame() {
+    return (
+        <div>
+            <div className='Board'>
+
+            </div>;
+            <div className='Pieces'>
+
+            </div>;
+            <div className='Timer'>
+
+            </div>;
+            <div className='End_Button'>
+
+            </div>;
+
+        </div>
+    )
+}
+
+export default PlayGame;
